2086416bad3b588b79550ad6ea955500f984f4cc
Allan <allan@pankman.com.br>
2021-04-08 23:18:04 -0300

    passing props

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,20 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+const repository = {
+    name: 'Nome',
+    description: 'Descrição',
+    link: 'link',
+}
+
 export function RepositoryList() {
     return (
         <section className="repository-list" >
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem />
-                <RepositoryItem />
-                <RepositoryItem />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
+                <RepositoryItem repository={repository} />
             </ul>
         </section>
     )
